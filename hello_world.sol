85885251237d7aa72de81319130a28306477451a
Stephen Yao <stephen@koho.ca>
2022-04-30 20:35:53 -0700

    add stored and return value

--- hello_world.sol
+++ hello_world.sol
@@ -1,7 +1,19 @@
 // My First Smart Contract 
-pragma solidity >=0.5.0 <0.7.0;
+pragma solidity >=0.7.0 <0.9.0; //Solidity package version
 contract HelloWorld {
-    function get()public pure returns (string memory){
-        return 'Hello Contracts';
+
+    uint storedValue; //Declare variables here and its type (unsigned integer)
+    
+    function storeNumber (uint x) public {
+        storedValue = x; //Set storedValue to x
     }
+
+    function retrieveNumer() public view returns (uint) {
+        return storedValue //Return storedValue from earlier
+    }
+
 }
+
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.3; //Here I specified the solidity version that I want to use
+
